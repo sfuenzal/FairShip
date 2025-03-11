0a7cdba85154549d6c529f65beb8648401188c20
Oliver Lantwin <oliver.lantwin@cern.ch>
2025-03-11 16:29:18 +0100

    fix(geometry): fix hardcoding of muon shield position

--- python/shipDet_conf.py
+++ python/shipDet_conf.py
@@ -237,6 +237,7 @@ def configure(run, ship_geo):
         MuonShield = ROOT.ShipMuonShield(
             in_params,
             ship_geo.cave.floorHeightMuonShield,
+            ship_geo.muShield.z,
             ship_geo.muShieldWithCobaltMagnet,
             ship_geo.muShieldStepGeo,
             ship_geo.hadronAbsorber.WithConstField,
