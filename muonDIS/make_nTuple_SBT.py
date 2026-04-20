a044c840b5164f909f7215411e16ce14290d4ac3
sfuenzal <sebastian.fuenzalida.garrido@cern.ch>
2026-04-20 20:10:01 +0200

    Implementation of PG

--- muonDIS/make_nTuple_SBT.py
+++ muonDIS/make_nTuple_SBT.py
@@ -30,6 +30,12 @@ parser.add_argument(
     help="path to muon background files",
     default="/eos/experiment/ship/simulation/bkg/MuonBack_2024helium/8070735",
 )
+parser.add_argument(
+    "-g",
+    "--generator",
+    help="type of generator, options are: MuonBack or PG",
+    default="MuonBack",
+)
 args = parser.parse_args()
 
 if args.testing_code:
@@ -65,6 +71,11 @@ output_tree.Branch("muon_vetoPoints", muon_vetoPoints)
 
 muon_UpstreamTaggerPoints = r.TClonesArray("UpstreamTaggerPoint")
 output_tree.Branch("muon_UpstreamTaggerPoints", muon_UpstreamTaggerPoints)
+
+if args.generator == "MuonBack":
+    reco_file_name = "ship.conical.MuonBack-TGeant4.root"
+elif args.generator == "PG":
+    reco_file_name = "ship.conical.PG_13-TGeant4_rec.root"
 
 h = {}
 h["PvPt_muon"] = r.TH2F(
@@ -227,7 +238,7 @@ for inputFolder in os.listdir(path):
     f = None
     try:
         f = r.TFile.Open(
-            os.path.join(path, inputFolder, "ship.conical.MuonBack-TGeant4.root"),
+            os.path.join(path, inputFolder, reco_file_name),
             "read",
         )
         tree = f.cbmsim
